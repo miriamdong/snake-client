ca3a2dbff32528132609887b1007c050fff5e3e8
miriamdong <miriamdong@gmai.com>
2021-03-10 22:25:30 +0000

    add move

--- input.js
+++ input.js
@@ -1,19 +1,33 @@
 const net = require('net');
+let connection;
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
-  stdin.resume();
   const handleUserInput = function(key) {
     if (key === '\u0003') {
       console.log('Thanks for using me, ciao!');
       process.exit();
+    } else if (key === 'w') {
+      console.log('up');
+      conn.write("Move: up");
+    } else if (key === 'a') {
+      console.log('left');
+      conn.write("Move: left");
+    } else if (key === 's') {
+      console.log('down');
+      conn.write("Move: down");
+    } else if (key === 'd') {
+      console.log('right');
+      conn.write("Move: right");
     }
   };
   stdin.on('data', handleUserInput);
   return stdin;
 };
+
 
 module.exports = {
   setupInput
--- play.js
+++ play.js
@@ -7,10 +7,10 @@ const {
   setupInput
 } = require('./input');
 
+
 /**
  * Establishes connection with the game server
  */
 
 console.log('Connecting ...');
-connect();
-setupInput();
+setupInput(connect());
