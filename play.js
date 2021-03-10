babe7e9959706c62a97441f866c2eb4b73f97e49
miriamdong <miriamdong@gmai.com>
2021-03-10 20:59:07 +0000

    add setupInput

--- play.js
+++ play.js
@@ -7,3 +7,23 @@ const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = function() {
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        console.log('Thanks for using me, ciao!');
+        process.exit();
+      }
+    });
+    return stdin;
+  };
+};
